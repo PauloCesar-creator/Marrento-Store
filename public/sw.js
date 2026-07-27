58d84570a36b2477b77dd98bf50930d070ff5e82
Paulo Cesar <paulocesarbasto11@gmail.com>
2026-07-27 11:05:30 -0300

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Simple fetch handler to meet PWA installability requirements
+});
